806210c878c250c4abad862ee772d6c1bcdd8c24
Levin Keller <post@levinkeller.de>
2018-06-21 17:10:27 +0200

    Prettier

--- tests/src/app.test.ts
+++ tests/src/app.test.ts
@@ -1,19 +1,21 @@
-import request = require('supertest');
+import request = require('supertest')
 import app from '../../src/app'
 import { assert } from 'chai'
 
 describe('POST /', function() {
   it('get Hello', function(done) {
     request(app)
-    .post('/')
-    .expect(200, done);
-  });
+      .post('/')
+      .expect(200, done)
+  })
 
   it('web3 connection is successful and get block number', async function() {
-    const { body: { blockNumber} } = await request(app)
-    .post('/getBlockNumber')
-    .expect(200);
-    assert.isNumber(blockNumber, 'Blocknumber is not a number.') 
+    const {
+      body: { blockNumber }
+    } = await request(app)
+      .post('/getBlockNumber')
+      .expect(200)
+    assert.isNumber(blockNumber, 'Blocknumber is not a number.')
     assert.isAbove(blockNumber, -1, 'BlockNumber is too low.')
-  });
-});
+  })
+})
