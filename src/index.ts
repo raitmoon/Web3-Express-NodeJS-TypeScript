05db26764efffe357fd0c81d8ba4b3012cdbe1f1
Tomo <tomo1026j@gmail.com>
2018-07-11 12:45:44 +0900

    add send method

--- src/index.ts
+++ src/index.ts
@@ -39,4 +39,27 @@ router.post('/getAccounts', async (req, res, next) => {
   res.send({accounts})
 })
 
+router.post('/send', async (req, res, next) => {
+  const senderId = '0x1C925b08ABCD86A5a157D610A283eA54B7183577'
+  const senderPassword = 'e659e73d38f2f152258f824c6427ae05143393aeb3d7d273e4ddd032f4b7a40a'
+  const duration = 30.0
+  const receiverId = '0x6F1328bf39BD5570db3fD876ba272089592Ee225'
+  const amount = web3.utils.toWei(req.body.amount.toString(), 'ether')
+  const result = await web3.eth.personal.unlockAccount(senderId, senderPassword, duration)
+  if (result === true) {
+    console.log('success')
+    const txResult = await web3.eth.sendTransaction({
+      from: senderId,
+      to: receiverId,
+      value: amount.toString()
+    })
+    const response = {txId: txResult.transactionHash}
+    res.send(response)
+  } else {
+    console.log('error')
+    const response = {txId: '12345678901234567890'}
+    res.send(response)
+  }
+})
+
 export default router
